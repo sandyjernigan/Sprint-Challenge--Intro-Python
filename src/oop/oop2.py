2319095b7131dd0f73318d0153c963b4f4e4e079
Sandy <sandy.jernigan@gmail.com>
2020-02-23 20:01:41 -0500

    Go through the vehicles list and print the result of calling drive() on each.

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -4,11 +4,12 @@
 # object is constructed.
 
 class GroundVehicle():
-    def __init__(self, num_wheels):
+    # num_wheels defaults to 4 if not specified
+    def __init__(self, num_wheels = 4):
         self.num_wheels = num_wheels
 
     # Method drive() should return "vroooom"
-    def drive():
+    def drive(self):
         return "vroooom"
 
 
@@ -19,7 +20,12 @@ class GroundVehicle():
 #
 # Override the drive() method in Motorcycle so that it returns "BRAAAP!!"
 
-# TODO
+class Motorcycle(GroundVehicle):
+    def __init__(self, num_wheels=2):
+        super().__init__(num_wheels=num_wheels)
+
+    def drive(self):
+        return "BRAAAP!!"
 
 vehicles = [
     GroundVehicle(),
@@ -31,4 +37,5 @@ vehicles = [
 
 # Go through the vehicles list and print the result of calling drive() on each.
 
-# TODO
+for e in vehicles:
+    print(e.drive())
